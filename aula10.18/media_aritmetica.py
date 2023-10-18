f0ae6f1d44b6901afb4fbffde66a3a381e7493f1
Lara Gabryella <laragabryella7@gmail.com>
2023-10-18 16:33:00 -0300

    media aritmetica ok

---
+++ aula10.18/media_aritmetica.py
@@ -0,0 +1,6 @@
+quantidade1 = input ('informe a quantidade da primeira caixa')
+quantidade2 = input ('informe a quantidade da segunda caixa')
+
+media_das_caixas = (int(quantidade1) + int(quantidade2))/2
+
+print ("A media da quantidade da caixa eh:", media_das_caixas)
