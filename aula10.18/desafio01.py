79b6f79db2f0fedb962eef905897aaf856685342
Lara Gabryella <laragabryella7@gmail.com>
2023-10-18 14:47:42 -0300

    desafio1 comitado

---
+++ aula10.18/desafio01.py
@@ -0,0 +1,8 @@
+#criando tres variaveis
+a = 5
+b = 30
+c = 20
+
+print (c - b - a) 
+
+
---
+++ aula10.18/não_nomeados.py
@@ -0,0 +1,12 @@
+#valores nomeados
+a = 1
+b = 1
+soma = a + b
+print (soma)
+
+#valores não nomeados
+print (1 + 1)
+
+#valores não nomeados não se propaga no seu codigo
+
+print (a + 10)
