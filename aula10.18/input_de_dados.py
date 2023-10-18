01109db2459f47d0ef37f505b293d877d3e06c7b
Lara Gabryella <laragabryella7@gmail.com>
2023-10-18 16:24:22 -0300

    input ok

---
+++ aula10.18/input_de_dados.py
@@ -0,0 +1,7 @@
+# Para criar uma interação com o usario utilizamos a função INPUT
+
+#1. todo INPUT deve ter seu valor associado a uma variavel
+
+nome = input('Digite seu nome aqui:')
+
+print(nome)
