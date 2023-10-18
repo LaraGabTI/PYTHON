35b91a6c7283c3945797a5c1c07719d30671e10c
Lara Gabryella <laragabryella7@gmail.com>
2023-10-18 16:23:25 -0300

    desafio2 concluido com sucesso

---
+++ aula10.18/desafio02.py
@@ -0,0 +1,10 @@
+
+
+idade1 = input('informe sua idade:')
+idade2 = input('informe a idade de alguém da sua familia:')
+nome = input('Digite seu nome')
+
+print (int(idade1) * int(idade2))
+print(nome)
+
+
