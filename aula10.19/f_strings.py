3d857f90a045a2708b5bfe473c759442c8026df3
Lara Gabryella <laragabryella7@gmail.com>
2023-10-19 15:48:26 -0300

    interpolação comitado

--- aula10.19/f_strings.py
+++ aula10.19/f_strings.py
@@ -1,3 +1,9 @@
 #intercpolação usar strings misturados com codigo
 #interpolando strings com codigo python
 
+#concatenação
+nome = input('Qual é o seu nome ')
+print ("Seja muito bem-vindo", nome)
+
+#interpolação
+print (f'Seja muito bem-vindo {nome}. Estavamos esperando por voce!')
