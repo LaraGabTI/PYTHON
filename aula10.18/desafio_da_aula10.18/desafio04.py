b34dc53df811a95d079b848cb1afad2b5cba05bc
Lara Gabryella <laragabryella7@gmail.com>
2023-10-19 15:02:42 -0300

    desafio concluido

--- aula10.18/desafio_da_aula10.18/desafio04.py
+++ aula10.18/desafio_da_aula10.18/desafio04.py
@@ -3,6 +3,6 @@ nota2 = input ('informe a nota 2')
 nota3 = input ('informe a nota 3')
 nota4 = input ('informe a nota 4')
 
-media_das_notas = (int(nota1) + int(nota2) + int(nota3) + int(nota4) )/4
+media_das_notas = (float(nota1) + float(nota2) + float(nota3) + float(nota4) )/4
 
 print ("A media das notas eh:", media_das_notas)
--- aula10.18/desafio_da_aula10.18/desafio07.py
+++ aula10.18/desafio_da_aula10.18/desafio07.py
@@ -4,6 +4,6 @@ numero1 = input('primeiro numero')
 numero2 = input('segundo numero')
 numero3 = input('terceiro numero')
 
-print (int(numero1)*2 * (int(numero2)/2 + int(numero3)))
+print (int(numero1)*2 * int(numero2)/2 + int(numero3))
 
 print ((int(numero1)*3 + int(numero3)) * int(numero2))
