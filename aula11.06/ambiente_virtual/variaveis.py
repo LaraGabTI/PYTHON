a062564b026c4a76b18a2d7b1229cd713984fa1b
Lara Gabryella <laragabryella7@gmail.com>
2023-11-06 16:31:38 -0300

    estudando as variações de variaveis comitada

---
+++ aula11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,21 @@
+# variaveis são usadas ara salvar algo na memoria do computador
+# o sinal de  = é utilizado para atribuição de valores
+
+var01 = "alguma coisa"
+print(var01)
+
+var01 = "outra coisa"
+print(var01)
+
+var01 = 10
+print(var01)
+
+var02 = "A" + "B" + "C"
+print(var02)
+
+var02 = "A" * 10
+print(var02)
+
+var02 = 10 * "Lara"
+print(var02)
+
