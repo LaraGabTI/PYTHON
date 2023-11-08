1a8f911ed70a97f4531f60f33bf41ccd58003a08
Lara Gabryella <laragabryella7@gmail.com>
2023-11-08 16:50:07 -0300

    atividades da aula do dia 11/08

---
+++ aula11.08/ambiente_virtual/atvdd01.py
@@ -0,0 +1,10 @@
+nome = "Lara Gab"
+altura = 1.50
+peso = 43
+imc = ... #depois informo esse valor
+imc = peso / (altura * altura)
+
+print (nome, "tem altura de:", altura, " \n pesa:", peso, "e seu IMC eh: \n", imc)
+
+#:.2f 
+print(f'{nome} tem {altura} de altura, pesa {peso} quilos e seu imc eh: \n {imc:.2f}')
