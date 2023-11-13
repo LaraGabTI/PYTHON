f39ca4b0f3c256ec512b0da4a8e43753caa5f048
Lara Gabryella <laragabryella7@gmail.com>
2023-11-13 14:51:56 -0300

    atividades da aula 11/09

---
+++ aula11.09/ambiente_virtual/atvdd01.py
@@ -0,0 +1,12 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimento e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+print(texto[0:5]) 
+print(texto[10:16])
+print(texto[20:31]) 
+print(texto[34:45]) 
+print(texto[61:66]) 
+print(texto[67:74]) 
+print(texto[-10:-1])
+print(texto[-19:-10])
+print(texto[-33:-22])
+print(texto[-48:-35])
---
+++ aula11.09/ambiente_virtual/atvdd02.py
@@ -0,0 +1,10 @@
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresa. Ambiente que proporciona conexão, troca de conhecimento e aprendizado compartilhado entre participantes, instrutores e empresa parceiras."
+
+print(texto[0:5]) 
+print(texto[6:9]) 
+print(texto[125:134]) 
+print(texto[17:19])
+print(texto[-18:-10])
+print(texto[61:66]) 
+print(texto[158:170]) 
+print(texto[170:183]) 
