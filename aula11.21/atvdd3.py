cbe28ae1f4c8a9c94ede0400587da2cf2ec124a2
Lara Gabryella <laragabryella7@gmail.com>
2023-11-22 16:39:41 -0300

    acrescentando lista a atvdd 3

--- aula11.21/atvdd3.py
+++ aula11.21/atvdd3.py
@@ -1,9 +1,14 @@
 soma = 0
+lista = []
+
 for i in range(3):
     idade = int(input("diga sua idade"))
+    lista.append(idade)
     soma += idade 
-    
+
+print(lista)
 media = soma/3
+
 if media > 0 and media < 25:
     print("turma jovem")
 elif media > 26 and media < 60:
