a11dee4677d7833f5cbae377aa138279268b2e22
Lara Gabryella <laragabryella7@gmail.com>
2023-11-30 11:37:43 -0300

    .

---
+++ aula29.11 revisap, join/revisao2.py
@@ -0,0 +1,9 @@
+#join - unir strins
+#monte uma data valida com a lista abaixo
+lista = ['Joao', 'Paulo', 'TI']
+nome = '-'.join(lista)
+print(nome)
+
+nome = "AMONTADA VALLEY"
+print(nome.capitalize())
+print(nome)
